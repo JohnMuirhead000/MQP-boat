24590c40aeb34650a917d9211a22b88e3875cd9d
shanestevenz <shanestevenz@gmail.com>
2023-02-08 16:49:45 -0500

    wokring net detection

--- src/vision/scripts/net_finder.py
+++ src/vision/scripts/net_finder.py
@@ -6,15 +6,21 @@ import cv2 # OpenCV library
 
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+# Load a model  
+#model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("runs/detect/train4/weights/best.pt")  # build a new model from scratch
 #model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-results = model.train(data="data.yaml", epochs=3)  # train the model
-results = model.val()  # evaluate model performance on the validation set
-results = model("test_net.jpg")  # predict on an image
-success = model.export(format="onnx")  # export the model to ONNX format
+#results = model.train(data="data.yaml", epochs=100)  # train the model
 
+#val = model.val()  # evaluate model performance on the validation set
+
+results = model.predict(source= "0",show=True)  # predict on an webcam...
+
+#results = model.predict(source="test/images", show=True) 
+
+
+k=input("press close to exit") 
 
 
